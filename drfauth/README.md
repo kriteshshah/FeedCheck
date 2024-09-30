41fe2a9255da1c60d9a6873d407d7509887f0317
kritesh_shah <kritesh.shah@9spl.com>
2024-09-30 18:19:37 +0530

    added new project

---
+++ drfauth/README.md
@@ -0,0 +1,1 @@
+# FeedRegarding post
---
+++ drfauth/users/filters.py
@@ -0,0 +1,13 @@
+import django_filters
+from .models import Post
+
+
+class PostFilter(django_filters.FilterSet):
+    class Meta:
+        model = Post
+        # fields = ['title', 'author']
+        ordering = ['-date_posted']
+        fields = {
+            'title': ['exact'],
+            'author': ['exact'],
+        }
